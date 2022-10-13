31eced7972272dfc6bcbde7af8be3c1865cc98e1
Jonathan Baker <35644651+SnicketySmock@users.noreply.github.com>
2022-10-12 18:22:39 -0700

    Added CpuUsageHelper.h

---
+++ Sources/Windows/CpuUsageHelper.h
@@ -0,0 +1,31 @@
+//==============================================================================
+/**
+@file       CpuUsageHelper.h
+
+@brief		Calculate the current CPU percentage
+
+@copyright  (c) 2018, Corsair Memory, Inc.
+			This source code is licensed under the MIT-style license found in the LICENSE file.
+
+**/
+//==============================================================================
+
+#pragma once
+#pragma comment(lib, "Pdh.lib")
+
+#include <winsock2.h>
+#include <windows.h>
+#include <pdh.h>
+
+class CpuUsageHelper
+{
+public:
+	CpuUsageHelper();
+	~CpuUsageHelper() {};
+	
+	int GetCurrentCPUValue();
+
+private:
+	PDH_HQUERY mCpuQuery;
+	PDH_HCOUNTER mCpuTotal;
+};
